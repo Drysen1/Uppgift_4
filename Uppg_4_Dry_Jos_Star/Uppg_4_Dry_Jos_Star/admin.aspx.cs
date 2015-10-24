cca6b9a1948dc57675c781f12976aa42059a46c8
Erik Drysén <erik.drysen@outlook.com>
2015-10-24 19:37:06 +0200

    Added admin page once again

--- Uppg_4_Dry_Jos_Star/Uppg_4_Dry_Jos_Star/admin.aspx.cs
+++ Uppg_4_Dry_Jos_Star/Uppg_4_Dry_Jos_Star/admin.aspx.cs
@@ -7,25 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Uppg_4_Dry_Jos_Star
 {
-    public partial class admin : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            string userType = "admin";
+	public partial class admin : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-            if (userType == "admin")
-            {
-
-            }
-            else
-            {
-                Response.Redirect("test.aspx");
-            }
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-
-        }
-    }
+		}
+	}
 }
