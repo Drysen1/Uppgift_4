2dc192c0c8741519b06e51d2f6f8aa4cc44ba152
Tremoten <malin.joshammar@gmail.com>
2015-10-29 12:42:44 +0100

    licenceringstest

---
+++ Uppg_4_Dry_Jos_Star/Uppg_4_Dry_Jos_Star/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Uppg_4_Dry_Jos_Star
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+
+        //metod som inte har en returtyp, property
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value;  }
+        }
+    }
+}
