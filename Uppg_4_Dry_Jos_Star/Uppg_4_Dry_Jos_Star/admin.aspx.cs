70331b95a789fabdf2b8e6709133d82d95eff2e6
Tremoten <malin.joshammar@gmail.com>
2015-11-02 13:43:17 +0100

    ...

--- Uppg_4_Dry_Jos_Star/Uppg_4_Dry_Jos_Star/admin.aspx.cs
+++ Uppg_4_Dry_Jos_Star/Uppg_4_Dry_Jos_Star/admin.aspx.cs
@@ -22,7 +22,7 @@ namespace Uppg_4_Dry_Jos_Star
             //    HtmlAnchor menuButton1 = (HtmlAnchor)Page.Master.FindControl("a1");
             //    menuButton.Visible = false;
             //    menuButton1.Visible = false;
-            //   // Response.Redirect("test.aspx");
+            //    // Response.Redirect("test.aspx");
             //}
             //else
             //{
@@ -33,6 +33,7 @@ namespace Uppg_4_Dry_Jos_Star
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            string userName = Request.QueryString["userName"];
             Response.Redirect("~/licencieringstest.aspx"); 
 
         }
