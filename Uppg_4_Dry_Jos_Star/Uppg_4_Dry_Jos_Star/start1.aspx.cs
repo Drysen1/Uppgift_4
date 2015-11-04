584b9141a81dfead9dabb98755870428fd357d4f
Erik Drysén <erik.drysen@outlook.com>
2015-11-04 22:46:45 +0100

    asdf

--- Uppg_4_Dry_Jos_Star/Uppg_4_Dry_Jos_Star/start1.aspx.cs
+++ Uppg_4_Dry_Jos_Star/Uppg_4_Dry_Jos_Star/start1.aspx.cs
@@ -16,7 +16,7 @@ namespace Uppg_4_Dry_Jos_Star
 {
     public partial class start1 : System.Web.UI.Page
     {
-        int userId = 2; //To get the user you would like, choose users id here. 
+        int userId = 5; //To get the user you would like, choose users id here. 
 
         protected void Page_Load(object sender, EventArgs e) //Session[username] gets set here
         {
@@ -142,7 +142,7 @@ namespace Uppg_4_Dry_Jos_Star
                     lblNextTestDate.Text = nextTestDate.ToString("yyyy-MM-dd");
                     toDoTest = "1";
                     btnStartTest.Enabled = true;
-                    //btnGoToOldTest.Enabled = false;
+                    btnGoToOldTest.Enabled = false;
                     return toDoTest;
                 }
                 else
@@ -151,7 +151,7 @@ namespace Uppg_4_Dry_Jos_Star
                     lblNextTestDate.Text = nextTestDate.ToString("yyyy-MM-dd");
                     toDoTest = "1";
                     btnStartTest.Enabled = false;
-                    //btnGoToOldTest.Enabled = false;
+                    btnGoToOldTest.Enabled = false;
                     return toDoTest;
                 }
             }
--- Uppg_4_Dry_Jos_Star/Uppg_4_Dry_Jos_Star/startButtons.js
+++ Uppg_4_Dry_Jos_Star/Uppg_4_Dry_Jos_Star/startButtons.js
@@ -1,5 +1,10 @@
 ﻿$(document).ready(function () {
-
+    alert("Laddat!");
+    var text = $("h1").Text();
+    alert("Texten borde vara här ----> " + text);
+    //if (text == "Inte godkänd") {
+    //    alert("Inte godkännnnndddd");
+    //}
     $("#ContentPlaceHolder1_btnGoToOldTest").hover(function () {
         //alert("test");
     });
@@ -12,4 +17,5 @@
 
 
     //var text = $("ContentPlaceHolder1_lblresult").Text();
-    //alert(text)
+//alert(text)
+
